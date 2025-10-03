029c0ee61cf3762d7153e355de369eff4514e010
1754riya <rmehta1754@gmail.com>
2025-10-03 18:29:26 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by Riya Mehta</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} RM</h3>
+          <h3>{year} RM</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
